4b6f40d154d8910ee129dd8b4d84ace4be5c5770
surafel <surafel@360ground.com>
2022-12-21 17:49:31 +0300

    assessment side effect

--- controllers/Course.js
+++ controllers/Course.js
@@ -52,7 +52,7 @@ router.get('/getAllModules/:courseId', async (req, res) => {
             res.status(200).send(JSON.parse(cacheResults));
 
         } else {
-            canvasAPI.getModules(req.params.courseId).then(async (response) => {
+            canvasAPI.getModules(req.params.courseId, req.query.studentId).then(async (response) => {
                 await redisClient.set(`modules/${req.params.courseId}`, JSON.stringify(response), {
                     EX: 30,
                   });
@@ -72,49 +72,19 @@ router.get('/getAllModules/:courseId', async (req, res) => {
     }
 });
 
-router.get('/getCourseExtraInfo/:courseId', async (req, res) => {
+
+router.get('/getQuizzes/:courseId', async (req, res) => {
     try {
 
-        // const cacheResults = await redisClient.get(`courseExtraInfo/${req.params.courseId}`);
-
-        // if (cacheResults) {
-        //     res.status(200).send(JSON.parse(cacheResults));
-
-        // } else {
-
-            // canvasAPI.getCourseFile(req.params.courseId).then((response) => {
-            //     let file = response.find((element) => element.display_name == "extraInfo.json");
+        canvasAPI.getQuizzes(req.params.courseId,include).then((response) => {
+            res.status(200).send({ status: true, message: response });
+        }).catch((errors) => {
+            res.status(200).send({
+                status: false,
+                message: errors.message
+            })
+        });
         
-            //     request.get(file.url, async function (error, response, body) {
-            //         if (!error && response.statusCode == 200) {
-            //             await redisClient.set(`courseExtraInfo/${req.params.courseId}`, body, {
-            //                 EX: 300,
-            //                 NX: true,
-            //               });
-            //             res.status(200).send(JSON.parse(body));
-            //         }
-            //     });
-        
-            // }).catch((errors) => {
-            //     res.status(200).send({
-            //         status: false,
-            //         message: errors.message
-            //     })
-            // });
-
-            pool.get_connection(qb => {
-                qb.select('*')
-                .where('courseId', req.params.courseId)
-                .get('tbl_course_extra_info', (err, response) => {
-                    qb.release();
-                    if (err) return res.status(200).send({ status: false, message: err.sqlMessage });
-                    res.status(200).send({ 
-                        status: true, message: response 
-                    });
-                });
-            });
-        // }
-
     } catch (err) {
         res.status(200).send({
             status: false,
@@ -123,8 +93,27 @@ router.get('/getCourseExtraInfo/:courseId', async (req, res) => {
     }
 });
 
+router.get('/getCourseExtraInfo/:courseId', async (req, res) => {
+    try {
+        pool.get_connection(qb => {
+            qb.select('*')
+            .where('courseId', req.params.courseId)
+            .get('tbl_course_extra_info', (err, response) => {
+                qb.release();
+                if (err) return res.status(200).send({ status: false, message: err.sqlMessage });
+                res.status(200).send({ 
+                    status: true, message: response 
+                });
+            });
+        });
 
-
+    } catch (err) {
+        res.status(200).send({
+            status: false,
+            message: err.message
+        })
+    }
+});
 
 // course extra information
 router.get('/getCourseExtraInfoDetail/:id', (req, res) => {
