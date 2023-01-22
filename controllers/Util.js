fcdb5f3dfe0b724e7a6eceb8f34ec2f69ca8f53a
surafel <surafel@360ground.com>
2023-01-22 13:14:53 +0300

    image manager

--- controllers/Util.js
+++ controllers/Util.js
@@ -5,6 +5,12 @@ const router = express.Router();
 const { deleteFiles } = require('../helpers/Files');
 const path = require('path')
 const fs = require("fs")
+const { convertBase64 } = require('../helpers/Files');
+
+const { pool } = require('../helpers/Db');
+
+const { uuid } = require('uuidv4');
+
 
 router.post('/contactUs', async (req, res) => {
 
@@ -37,6 +43,120 @@ router.post('/contactUs', async (req, res) => {
 });
 
 
+router.post('/createImage', async(req, res) => {
+    req.body.id = uuid().replace('-', '');
+
+    let image = req.body.image;
+
+    delete req.body.image;
+
+    try {
+    
+        pool.get_connection(qb => {
+            qb.insert('tbl_images_gallery', req.body, async (err) => {
+                qb.release();
+                if (err) {
+                    return res.send({ status: false, message: err.sqlMessage });
+
+                } else {
+
+                    let result = await convertBase64(image, req.body.id,`images/slide`, true, true);
+
+                    if(result.status){
+                        res.send({ status: true, message: 'Uploaded successfully.' });
+
+                    } else {
+                        res.send(result);
+
+                    }
+                }    
+            });
+        });
+        
+    } catch(err){
+        res.status(200).send({ status: false, message: err.message });
+    }
+});
+
+router.post('/updateImage', async(req, res) => {
+    try {
+
+        pool.get_connection(qb => {
+            qb.update('tbl_images_gallery', req.body, { id: req.body.id }, (err) => {
+                qb.release();
+                if (err) return res.send({ status: false, message: err.sqlMessage });
+                res.send({ status: true, message: 'Updated successfully.' });
+            });
+        });
+
+    } catch(err){
+        res.status(200).send({ status: false, message: err.message });
+    }
+});
+
+router.post('/sortImage', async(req, res) => {
+    try {
+        pool.get_connection(qb => {
+            let select = "*,CONCAT('images/slide/',id) As url";
+
+            qb.select(select, false)
+            .from('tbl_images_gallery')
+            .order_by('updatedAt',req.body.sortBy)
+            .get((err, response) => {
+                qb.release();
+                if (err) return res.status(200).send({ status: false, message: err.sqlMessage });
+                res.status(200).send({ 
+                    status: true, message: response 
+                });
+            });
+        });
+
+    } catch(err){
+        res.status(200).send({ status: false, message: err.message });
+    }
+});
+
+router.post('/searchImage', async(req, res) => {
+    try {
+
+        pool.get_connection(qb => {
+            let select = "*,CONCAT('images/slide/',id) As url";
+
+            qb.select(select, false)
+            .from('tbl_images_gallery')
+            .or_like('title', req.body.title)
+            .get((err, response) => {
+                qb.release();
+                if (err) return res.status(200).send({ status: false, message: err.sqlMessage });
+                res.status(200).send({ 
+                    status: true, message: response 
+                });
+            });
+        });
+
+    } catch(err){
+        res.status(200).send({ status: false, message: err.message });
+    }
+});
+
+
+router.post('/deleteImage', (req, res) => {
+    try {
+        pool.get_connection(qb => {
+            qb.or_where_in('id', req.body.ids)
+            qb.delete('tbl_images_gallery', (err) => {
+                qb.release();
+                if (err) return res.status(200).send({ status: false, message: err.message });
+
+                res.status(200).send({ status: true, message: 'Image deleted successfully.' });
+            });
+        });
+  
+    } catch(err){
+        res.status(200).send({ status: false, message: err.message });
+    }
+});
+
 // general file delete endpoint
 router.post('/deleteFiles', async(req, res) => {
     let result = await deleteFiles(req, res);
@@ -45,23 +165,26 @@ router.post('/deleteFiles', async(req, res) => {
 
 
 router.get('/getSlidePhotos', async(req, res) => {
-    const directoryPath = path.join(process.cwd(), 'public/images/slide');
+    try {
+        pool.get_connection(qb => {
+            let select = "*,CONCAT('images/slide/',id) As url";
 
-    fs.readdir(directoryPath, function (err, files) {
-        if (err) {
-            res.status(200).send({ status: false, message: err.message })
-
-        } else {
-            let newFiles = [];
-
-            files.forEach((file) => {
-                newFiles.push(`images/slide/${file}`);
+            qb.select(select, false)
+            .from('tbl_images_gallery')
+            .order_by('updatedAt','desc')
+            .get((err, response) => {
+                qb.release();
+                if (err) return res.status(200).send({ status: false, message: err.sqlMessage });
+                res.status(200).send({ 
+                    status: true, message: response 
+                });
             });
+        });
 
-            res.status(200).send({ status: true, message: newFiles })
-        }
+    } catch(err){
+        res.status(200).send({ status: false, message: err.message });
+    }
 
-    });
 });
 
 
--- helpers/Files.js
+++ helpers/Files.js
@@ -1,54 +1,50 @@
 const fs = require("fs")
 const Jimp = require("jimp");
 const base64 = require('node-base64-image');
-const path = require('path')
+const path = require('path');
+
+const { uuid } = require('uuidv4');
 
 const staticPath = path.join(process.cwd(),'public')
 
 // convert base64 string into actual file.
-const convertBase64 = async (data, fileName, directory = 'ids',isImage = true) => {
+const convertBase64 = async (data, fileName, directory = 'ids',isImage = true, isSlideImage = false) => {
+    
     try {
 
-        var url = `${staticPath}/uploads/${directory}/`;
+        var url = isSlideImage ? `${staticPath}/images/slide/` : `${staticPath}/uploads/${directory}/`;
+
         data = data.split("base64,");
 
-        // if (data[0].includes('data:image')) {
+        if (!fs.existsSync(url)) {
+            fs.mkdirSync(url, { recursive: true });   
+        } 
 
-            if (!fs.existsSync(url)) {
-                fs.mkdirSync(url, { recursive: true });   
-            } 
+        const buffer = Buffer.from(data[1], "base64");
+        let result = await base64.decode(buffer, { fname: `${url}${fileName}`, ext: isImage ? 'jpeg' : 'pdf' });
 
-            const buffer = Buffer.from(data[1], "base64");
-            let result = await base64.decode(buffer, { fname: `${url}${fileName}`, ext: isImage ? 'jpeg' : 'pdf' });
-
-            if(result == 'file written successfully to disk'){
-                if(isImage){
-                    Jimp.read(`${url}${fileName}.jpeg`)
-                    .then(image => {
-                        return image.quality(40).write(`${url}${fileName}.jpeg`);
-                        
-                    }).catch(err => {
-                        return {
-                            status: false, message: err.message
-                        };
-                    });
-                } else {
+        if(result == 'file written successfully to disk'){
+            if(isImage){
+                Jimp.read(`${url}${fileName}.jpeg`)
+                .then(image => {
+                    return image.quality(40).write(`${url}${fileName}.jpeg`);
+                    
+                }).catch(err => {
                     return {
-                        status: true, message: 'successfully upload file.'
+                        status: false, message: err.message
                     };
-                }
-                
+                });
             } else {
                 return {
-                    status: false, message: 'unable to upload file.'
+                    status: true, message: 'successfully upload file.'
                 };
             }
-
-        // } else {
-        //     return {
-        //         status: false, message: 'Only image is allowed to upload. please try again.'
-        //     }
-        // }
+            
+        } else {
+            return {
+                status: false, message: 'unable to upload file.'
+            };
+        }
 
     } catch(err) {
         return {
